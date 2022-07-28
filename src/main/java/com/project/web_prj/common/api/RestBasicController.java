0532910f98d99745c68b4692ed70f218894bcb85
jyeonki <kay8825@naver.com>
2022-07-28 14:23:02 +0900

    [220728] # Third interim commit

--- src/main/java/com/project/web_prj/common/api/RestBasicController.java
+++ src/main/java/com/project/web_prj/common/api/RestBasicController.java
@@ -2,8 +2,10 @@ package com.project.web_prj.common.api;
 
 import com.project.web_prj.board.domain.Board;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 // jsp 뷰포워딩을 하지 않고 클라이언트에게 JSON 데이터를 전송함
 @RestController
@@ -34,5 +36,41 @@ public class RestBasicController {
 
         return foods;
     }
+
+    // post 요청처리
+    @PostMapping("/api/join")
+    public String joinPost(@RequestBody List<String> info) {
+        // @RequestBody 안 붙이면 자동으로 @RequestParam 처리가 되어서 보내진다
+        //  JSON으로 받으려면 - @RequestBody
+        log.info("/api/join POST! - {}", info);
+
+        return "POST OK";
+    }
     
+    // put 요청처리
+    @PutMapping("/api/join")
+    public String joinPut(@RequestBody Board board) {
+        log.info("/api/join PUT! - {}", board);
+
+        return "PUT OK";
+    }
+    
+    // delete 요청처리
+    @DeleteMapping("/api/join")
+    public String joinDelete() {
+        log.info("/api/join DELETE!");
+
+        return "DELETE OK";
+    }
+
+    // @RestController에서 뷰포워딩하기
+    @GetMapping("/hoho")
+    public ModelAndView hoho() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+
+        return mv;
+    }
+
+    // @ResponseBody // 리턴데이터가 뷰포워딩이 아닌 JSON으로 전달됨. (@RestController 로 작동)
 }
