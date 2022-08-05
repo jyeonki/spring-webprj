4d6f1d7551dc89cc134bed733853e252d016b4a3
jyeonki <kay8825@naver.com>
2022-08-05 13:45:14 +0900

    [220805] # First interim commit

---
+++ src/main/java/com/project/web_prj/member/controller/KakaoController.java
@@ -0,0 +1,41 @@
+package com.project.web_prj.member.controller;
+
+import com.project.web_prj.member.domain.OAuthValue;
+import com.project.web_prj.member.service.KakaoService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import static com.project.web_prj.member.domain.OAuthValue.*;
+
+@Controller
+@Log4j2
+@RequiredArgsConstructor
+public class KakaoController {
+
+    private final KakaoService kakaoService;
+
+    @GetMapping("/kakao-test") // uri == jsp 이름이 같으면 바로 열린다
+    public void kakaoTest(Model model) {
+        log.info("forward to kakao-test.jsp!");
+        model.addAttribute("appKey", KAKAO_APP_KEY);
+        model.addAttribute("redirectUri", KAKAO_REDIRECT_URI);
+    }
+
+    // 카카오 인증서버가 보내준 인가코드를 받아서 처리할 메서드
+    @GetMapping(KAKAO_REDIRECT_URI)
+    public String kakaoLogin(String code) throws Exception {
+        log.info("{} GET! code - {}", KAKAO_REDIRECT_URI, code);
+
+        // 인가코드를 통해 액세스토큰 발급받기
+        // 우리서버에서 카카오서버로 통신을 해야함
+        String accessToken = kakaoService.getAccessToken(code);
+
+        // 엑세스 토큰을 통해 사용자 정보 요청 (프로필사진, 닉네임 등)
+        kakaoService.getKakaoUserInfo(accessToken);
+
+        return "";
+    }
+}
---
+++ src/main/java/com/project/web_prj/member/domain/OAuthValue.java
@@ -0,0 +1,9 @@
+package com.project.web_prj.member.domain;
+
+public interface OAuthValue {
+
+    // 인터페이스에 필드를 만들면 상수처리된다
+
+    String KAKAO_APP_KEY = "3b6a25446e75141474e520205cb08630";
+    String KAKAO_REDIRECT_URI = "/oauth/kakao";
+}
